c0654b21a12b82a7017c3b9383e8e3c3b80747bd
Jenil Mewada <jenil@iitk.ac.in>
2018-11-11 06:14:15 +0530

    Profile page update

--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -10,13 +10,14 @@ import firebase from 'firebase';
 */
 @Injectable()
 export class UserProvider {
-  firedata = firebase.database().ref('/chatusers');
+  firedata = firebase.database().ref('/users');
   constructor(public afireauth: AngularFireAuth) {
-   
   }
 
   /*
+
   Adds a new user to the system.
+
   Called from - signup.ts
   Inputs - The new user object containing the email, password and displayName.
   Outputs - Promise.
@@ -49,6 +50,15 @@ export class UserProvider {
     return promise;
   }
 
+  /*
+
+  For resetting the password of the user.
+  Called from - passwordreset.ts
+  Inputs - email of the user.
+  Output - Promise.
+  
+   */
+
   passwordreset(email) {
     var promise = new Promise((resolve, reject) => {
       firebase.auth().sendPasswordResetEmail(email).then(() => {
@@ -60,27 +70,70 @@ export class UserProvider {
     return promise;
   }
 
+  /*
+  
+  For updating the users collection and the firebase users list with
+  the imageurl of the profile picture stored in firebase storage.
+
+  Called from - profilepic.ts
+  Inputs - Url of the image stored in firebase.
+  OUtputs - Promise.
+  
+  */
+
   updateimage(imageurl) {
+      var promise = new Promise((resolve, reject) => {
+          this.afireauth.auth.currentUser.updateProfile({
+              displayName: this.afireauth.auth.currentUser.displayName,
+              photoURL: imageurl      
+          }).then(() => {
+              firebase.database().ref('/users/' + firebase.auth().currentUser.uid).update({
+              displayName: this.afireauth.auth.currentUser.displayName,
+              photoURL: imageurl,
+              uid: firebase.auth().currentUser.uid
+              }).then(() => {
+                  resolve({ success: true });
+                  }).catch((err) => {
+                      reject(err);
+                  })
+          }).catch((err) => {
+                reject(err);
+             })  
+      })
+      return promise;
+  }
+
+  getuserdetails() {
     var promise = new Promise((resolve, reject) => {
-        this.afireauth.auth.currentUser.updateProfile({
-            displayName: this.afireauth.auth.currentUser.displayName,
-            photoURL: imageurl      
-        }).then(() => {
-            firebase.database().ref('/users/' + firebase.auth().currentUser.uid).update({
-            displayName: this.afireauth.auth.currentUser.displayName,
-            photoURL: imageurl,
-            uid: firebase.auth().currentUser.uid
-            }).then(() => {
-                resolve({ success: true });
-                }).catch((err) => {
-                    reject(err);
-                })
-        }).catch((err) => {
-              reject(err);
-           })  
+    this.firedata.child(firebase.auth().currentUser.uid).once('value', (snapshot) => {
+      resolve(snapshot.val());
+    }).catch((err) => {
+      reject(err);
+      })
     })
     return promise;
-}
+  }
 
+  updatedisplayname(newname) {
+    var promise = new Promise((resolve, reject) => {
+      this.afireauth.auth.currentUser.updateProfile({
+      displayName: newname,
+      photoURL: this.afireauth.auth.currentUser.photoURL
+    }).then(() => {
+      this.firedata.child(firebase.auth().currentUser.uid).update({
+        displayName: newname,
+        photoURL: this.afireauth.auth.currentUser.photoURL,
+        uid: this.afireauth.auth.currentUser.uid
+      }).then(() => {
+        resolve({ success: true });
+      }).catch((err) => {
+        reject(err);
+      })
+      }).catch((err) => {
+        reject(err);
+    })
+    })
+    return promise;
+  }
 
 }
