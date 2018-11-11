08c926115d66435b767686d3903375e5f823631b
Arpit Gupta <arpitguptarag@gmail.com>
2018-11-11 12:28:36 +0530

    group members add/remove

--- src/providers/groups/groups.ts
+++ src/providers/groups/groups.ts
@@ -66,6 +66,8 @@ export class GroupsProvider {
           for (var key in temp) {
             this.currentgroup.push(temp[key]);
           }
+          this.currentgroupname = groupname;
+          this.events.publish('gotintogroup');
         }
       })
     }
@@ -86,6 +88,55 @@ export class GroupsProvider {
       })
     })
     return promise;
+  }
+
+  getgroupimage() {
+    return new Promise((resolve, reject) => {
+      this.firegroup.child(firebase.auth().currentUser.uid).child(this.currentgroupname).once('value', (snapshot) => {
+        this.grouppic = snapshot.val().groupimage;
+        resolve(true);
+    })
+    })
+    
+  }
+ 
+  addmember(newmember) {
+    this.firegroup.child(firebase.auth().currentUser.uid).child(this.currentgroupname).child('members').push(newmember).then(() => {
+      this.getgroupimage().then(() => {
+        this.firegroup.child(newmember.uid).child(this.currentgroupname).set({
+          groupimage: this.grouppic,
+          owner: firebase.auth().currentUser.uid,
+          msgboard: ''
+        }).catch((err) => {
+          console.log(err);
+        })
+      })
+      this.getintogroup(this.currentgroupname);
+    })    
+  } 
+
+  deletemember(member) {           
+    this.firegroup.child(firebase.auth().currentUser.uid).child(this.currentgroupname)
+      .child('members').orderByChild('uid').equalTo(member.uid).once('value', (snapshot) => {
+        snapshot.ref.remove().then(() => {
+          this.firegroup.child(member.uid).child(this.currentgroupname).remove().then(() => {
+            this.getintogroup(this.currentgroupname);
+          })
+        })
+      })
+  }
+
+  getgroupmembers() {
+    this.firegroup.child(firebase.auth().currentUser.uid).child(this.currentgroupname).once('value', (snapshot) => {
+      var tempdata = snapshot.val().owner;
+      this.firegroup.child(tempdata).child(this.currentgroupname).child('members').once('value', (snapshot) => {
+        var tempvar = snapshot.val();
+        for (var key in tempvar) {
+          this.currentgroup.push(tempvar[key]);
+        }
+      })
+    })
+    this.events.publish('gotmembers');
   }
 
 }
